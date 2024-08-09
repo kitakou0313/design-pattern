bc7b2c710870818f026c6c3ff5b0cdb9274b73b0
kitakou0313 <kitakou0313@gmail.com>
2024-08-09 22:03:59 +0900

    add:define builder interface

--- src/Builder/builders/Bulder.java
+++ src/Builder/builders/Bulder.java
@@ -1,9 +1,18 @@
 package Builder.builders;
 
+import Builder.cars.CarType;
+import Builder.components.Engine;
+import Builder.components.GPSNavigator;
+import Builder.components.Transmission;
+
 /**
  * Bulder
  */
 public interface Bulder {
-
+    void setCarType(CarType type);
+    void setSeats(int seats);
+    void setEngine(Engine engine);
+    void setTransmission(Transmission transmission);
+    void setGPSNavigator(GPSNavigator gpsNavigator);
     
 }
