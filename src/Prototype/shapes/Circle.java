e15206a732948e7e982c28b82c2f7b68831c3f8c
kitakou0313 <kitakou0313@gmail.com>
2024-08-15 22:27:57 +0900

    add:Define Circle

---
+++ src/Prototype/shapes/Circle.java
@@ -0,0 +1,29 @@
+package Prototype.shapes;
+
+public class Circle extends Shape {
+    public int radius;
+
+    public Circle() {
+    }
+
+    public Circle(Circle target) {
+        super(target);
+        if (target != null) {
+            this.radius = target.radius;
+        }
+    }
+
+    @Override
+    public Shape clone() {
+        return new Circle(this);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Circle) || !super.equals(obj)) {
+            return false;
+        }
+        Circle shape2 = (Circle)obj;
+        return shape2.radius == this.radius;
+    }
+}
